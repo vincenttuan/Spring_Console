6e354560eaa6bb2688b83b3088e8360829c05c32
student <student@192.168.2.15>
2019-06-14 19:39:49 +0800

    Spring

--- src/main/java/hello/spring/test/Test.java
+++ src/main/java/hello/spring/test/Test.java
@@ -1,7 +1,15 @@
 package hello.spring.test;
 
+import hello.spring.beans.IHelloSpring;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
     public static void main(String[] args) {
-        
-    }
+        ApplicationContext context = 
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        IHelloSpring hello = (IHelloSpring)context.getBean("hello");
+        hello.print();
+    }    
 }
+
