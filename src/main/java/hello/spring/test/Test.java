d13b6a7768e11cec4c25accdcdb8daf14b20efaf
student <student@192.168.2.15>
2019-06-14 19:48:22 +0800

    Spring

--- src/main/java/hello/spring/test/Test.java
+++ src/main/java/hello/spring/test/Test.java
@@ -3,11 +3,13 @@ package hello.spring.test;
 import hello.spring.beans.IHelloSpring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        ApplicationContext context = 
-                new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext context = new FileSystemXmlApplicationContext("C:\\temp\\applicationContext.xml");
+        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/applicationContext.xml");
         IHelloSpring hello = (IHelloSpring)context.getBean("hello");
         hello.print();
     }    
