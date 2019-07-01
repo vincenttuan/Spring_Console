6611f0db10b318b2c8dceca99905df087fb7416d
student <student@192.168.2.15>
2019-07-01 21:36:15 +0800

    Spring

--- src/main/java/stock/orm/test/Investor_CRUD.java
+++ src/main/java/stock/orm/test/Investor_CRUD.java
@@ -4,33 +4,38 @@ package stock.orm.test;
 import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import stock.orm.dao.IFundDao;
 import stock.orm.dao.IInvestorDao;
-import stock.orm.dao.ITraderDao;
+import stock.orm.model.Fund;
 import stock.orm.model.Investor;
 import stock.orm.model.Trader;
-import static stock.orm.test.Trader_CRUD.queryAll;
 
 public class Investor_CRUD {
-    static ApplicationContext context;
-    static IInvestorDao investorDao;
+    private static ApplicationContext context;
+    private static IInvestorDao investorDao;
+    private static IFundDao fundDao;
     static {
         context = new ClassPathXmlApplicationContext("applicationContext_stock.xml");
         investorDao = (IInvestorDao)context.getBean("investorDao");
+        fundDao = (IFundDao)context.getBean("fundDao");
     }
     public static void main(String[] args) {
-        //add();
-        queryAll().stream()
-                .forEach(e -> {
-                    System.out.printf("%s %f %d %s\n", e.getName(), e.getUnits(), e.getNetValue(), e.getFund());
-                });
+        add("Happy", 10000, 8, "A");
+//        queryAll().stream()
+//                .forEach(e -> {
+//                    System.out.printf("%s %.0f %d %s\n", e.getName(), e.getUnits(), e.getNetValue(), e.getFund().getName());
+//                });
     }
     
-    public static void add() {
-        Investor inv1 = new Investor("Happy");
-        Investor inv2 = new Investor("Lucky");
-        investorDao.create(inv1);
-        investorDao.create(inv2);
+    public static void add(String investName, double units, int netValue, String fundName) {
+        Fund fund = fundDao.find(fundName);
+        Investor inv = new Investor(investName);
+        inv.setNetValue(netValue);
+        inv.setUnits(units);
+        inv.setFund(fund);
+        investorDao.create(inv);
     }
+    
     
     public static List<Investor> queryAll() {
         return investorDao.queryAll(Investor.class);
